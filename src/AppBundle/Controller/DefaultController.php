9d38adbbc5cf97b916dd2bebb8c110b93b712287
Jherel Córdova <jherel.cordova@netandco.es>
2015-11-15 23:16:45 +0100

    funcion send Contact Email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,31 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        
+        return $this->render('default/index.html.twig');
+    }
+
+    /*
+    * @Route("", name="contactEmail")
+    */
+    public function sendContactEmailAction(Request $request)
+    {
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Contact Email')
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->renderView(
+                    // app/Resources/views/emails/contact.html.twig
+                    'emails/contact.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+
+        return $this->render('default/index.html.twig');
     }
 }
