421070b56a4958bc8b61e3838f5654e6b5cc8955
Jherel Córdova <jherel.cordova@netandco.es>
2016-01-07 14:13:05 +0100

    Estructura terminada, reestructura de imagenes

--- web/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-function muestra_oculta(id){
-    if (document.getElementById){ //se obtiene el id
-    var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
-    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
-    }
-}
-window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
-    muestra_oculta('contacto');/* "contacto" es el nombre que le dimos al DIV */
-}
