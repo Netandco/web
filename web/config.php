66e01ccf4d171022c050dd8342cdcf88889323d5
Jherel Córdova <jherel.cordova@netandco.es>
2016-02-13 11:35:55 +0100

    modificacion sig en el config

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
-
+/*
 if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
-/*
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
