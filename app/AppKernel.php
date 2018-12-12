ac623f91dccb6222b2d9272d6c428dd7f2c3cef7
David Sánchez <davidmiguel.sanchez@gmail.com>
2018-12-12 21:22:32 +0100

    Update app/AppKernel.php
    Trying to solve a problem related to environment variable

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
         );
 
         /* (true || in_array($this->getEnvironment(), ['dev', 'test'], true)) */
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (true || in_array($this->getEnvironment(), array('dev', 'test')), true) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
