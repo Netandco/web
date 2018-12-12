a5eccf0d6cb2e266c2fd514a4171640e46ebd4ce
David Sánchez <davidmiguel.sanchez@gmail.com>
2018-12-12 21:18:42 +0100

    Update app/AppKernel.php
    test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
         );
 
-        /* if (in_array($this->getEnvironment(), array('dev', 'test'))) */
-        if (true || in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        /* (true || in_array($this->getEnvironment(), ['dev', 'test'], true)) */
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
